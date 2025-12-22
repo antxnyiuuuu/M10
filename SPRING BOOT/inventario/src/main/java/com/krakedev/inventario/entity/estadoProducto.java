5f22f460eb491895fd36a62643515d34f907fc7b
antxnyiuuuu <antonychaguamate@gmail.com>
2025-12-22 09:21:27 -0500

    Conexion con mysql

---
+++ SPRING BOOT/inventario/src/main/java/com/krakedev/inventario/entity/estadoProducto.java
@@ -0,0 +1,6 @@
+package com.krakedev.inventario.entity;
+
+public enum estadoProducto {
+    DISPONIBLE, NO_DISPONIBLE
+
+}
---
+++ SPRING BOOT/inventario/src/main/java/com/krakedev/inventario/entity/producto.java
@@ -0,0 +1,11 @@
+package com.krakedev.inventario.entity;
+
+public class producto {
+    private long idproducto;
+    private String nombreProducto;
+    private String descripcion;
+    private double precio;
+    private int cantidad;
+    private estadoProducto estadoProducto;
+
+}
