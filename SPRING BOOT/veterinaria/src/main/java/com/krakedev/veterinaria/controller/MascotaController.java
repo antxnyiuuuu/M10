fc2db480af15b2a06f23709f2723c767087f98ea
antxnyiuuuu <antonychaguamate@gmail.com>
2026-01-13 22:02:16 -0500

    reto 15

--- SPRING BOOT/veterinaria/src/main/java/com/krakedev/veterinaria/controller/MascotaController.java
+++ SPRING BOOT/veterinaria/src/main/java/com/krakedev/veterinaria/controller/MascotaController.java
@@ -15,7 +15,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -31,13 +33,14 @@ public class MascotaController {
         return ResponseEntity.ok(mascotas);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("buscar/id/{id}")
     public ResponseEntity<Mascota> obtenerMascotaPorId(@PathVariable Long id) {
         Optional<Mascota> mascota = mascotaService.buscarPorId(id);
         return mascota.map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("buscar/nombre/{nombre}")
     public ResponseEntity<Mascota> obtenerMascotaPorNombre(@PathVariable String nombre) {
         Optional<Mascota> mascota = mascotaService.buscarPorNombre(nombre);
         return mascota.map(ResponseEntity::ok)
@@ -50,6 +53,7 @@ public class MascotaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevaMascota);
     }
 
+    @PutMapping("/actualizar/{id}")
     public ResponseEntity<Mascota> actualizarMascota(@PathVariable Long id, @RequestBody Mascota mascota) {
         try {
             Mascota mascotaActualizada = mascotaService.actualizarMascota(id, mascota);
@@ -59,6 +63,7 @@ public class MascotaController {
         }
     }
 
+    @DeleteMapping("/eliminar/{id}")
     public ResponseEntity<Void> eliminarMascota(@PathVariable Long id) {
         try {
             mascotaService.eliminarMascota(id);
